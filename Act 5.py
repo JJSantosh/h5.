1c57233ce6e7fea393fe6cb74e84635b94f0f7b8
JJSantosh <jairaj.hp@gmail.com>
2025-01-07 18:25:15 +0530

    Add files via upload

---
+++ Act 5.py
@@ -0,0 +1,11 @@
+a=int(input("Enter the temperature:"))
+if a==50:
+    print("Its summer season so you have to wear light clothes")
+elif a==40:
+    print("Its spring season so you can wear whatever clothes you want")
+elif a==30:
+    print("Its fall so you have to wear proper clothes")
+elif a==20:
+    print("Its rainy season so you have to wear slightly warm clothes")
+else:
+    print("Its winter season so you have to wear really warm clothes")
